fbd478f5c72ddcbb3b2a6711a5c0b308565d46da
valeriiia2003 <leraspasibukhova@ukr.net>
2024-11-27 19:52:38 +0000

    Log connection task was completed (task 4)

---
+++ 04_LogConnection/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _04_LogConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection = Logger.LogFilePath;
+            Console.WriteLine("Path : {0}", connection);
+
+            string messageToLogger = "Data added";
+            Logger.MessageLogWriter(messageToLogger);
+
+            int counter = Logger.CountMessages();
+            Console.WriteLine("Count: {0}", counter);
+
+            string errorOccurred = "Bugs were fixed";
+            Logger.MessageLogWriter(errorOccurred);
+
+            Console.WriteLine("Counter : {0}", Logger.CountMessages());
+
+            Console.ReadKey();
+        }
+    }
+    public static class Logger
+    {
+        private static string logFilePath;
+
+        private static readonly List<string> Messages;
+
+        public static int CountMessages()
+        {
+            return Messages.Count();
+        }
+        public static string LogFilePath
+        {
+            get { return logFilePath; }
+            private set { logFilePath = value; }
+        }
+
+        static Logger()
+        {
+            logFilePath = "path to database connection";
+            Messages = new List<string>();
+        }
+
+        public static void MessageLogWriter(string message)
+        {
+            string logEntry = $"[{DateTime.Now}] - {message}";
+            Messages.Add(logEntry);
+
+            Console.WriteLine($"Log message was added: \n -- [{message}] --");
+        }
+    }
+}
