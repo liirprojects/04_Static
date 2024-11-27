94e0fbc0e580a5052c0d383011f88441e16e226d
valeriiia2003 <leraspasibukhova@ukr.net>
2024-11-27 17:46:54 +0000

    Project 1 is created and completed

---
+++ 01_MathOperations/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01_MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = 5.2;
+            double returnSquare = MathUtils.CalculateCircleArea(r);
+
+            Console.WriteLine("Circle square if the radius is [{0}] equals [{1:F3}]", r, returnSquare);
+
+            Console.ReadKey();
+        }
+    }
+    public static class MathUtils
+    {
+        public const double Pi = 3.14;
+
+        public static double CalculateCircleArea(double radius)
+        {
+            double square = Pi + Math.Pow(radius, 2);
+            return square;
+        }
+    }
+}
