ca514895cf7d8201996c833e7f73da1590041383
valeriiia2003 <leraspasibukhova@ukr.net>
2024-11-27 19:49:01 +0000

    Second task was completed

---
+++ 02_ClassInstance/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02_ClassInstance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter1 = new Counter();
+
+            Counter counter2 = new Counter();
+
+            Console.WriteLine(Counter.Counter_value);
+
+            Counter counter3 = new Counter();
+
+            Console.WriteLine(Counter.Counter_value);
+        }
+    }
+    class Counter
+    {
+        private static int counter_value;
+        public static int Counter_value
+        {
+            get { return counter_value; }
+            private set { counter_value = value; }
+        }
+
+        public Counter()
+        {
+            counter_value++;
+        }
+    }
+}
